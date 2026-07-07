3daa2231c9e9d1e88b61e09af51fe06fe5c5c124
adrianmwc <128684611+adrianmwc@users.noreply.github.com>
2026-07-07 11:04:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,26 +10,14 @@ const ASSETS = [
   './jspdf.umd.min.js',
   './images/icon.png',
   // --- MISSION IMAGES (Numbered 01-20) ---
-  './images/01-fountain.jpg',
-  './images/02-statue.jpg',
-  './images/03-library.jpg',
-  './images/04-bust.jpg',
-  './images/05-garden.jpg',
-  './images/06-bench.jpg',
-  './images/07-clock.jpg',
-  './images/08-gates.jpg',
-  './images/09-tree.jpg',
-  './images/10-science.jpg',
-  './images/11-stage.jpg',
-  './images/12-gallery.jpg',
-  './images/13-bell.jpg',
-  './images/14-gym.jpg',
-  './images/15-plants.jpg',
-  './images/16-arch.jpg',
-  './images/17-pond.jpg',
-  './images/18-mural.jpg',
-  './images/19-well.jpg',
-  './images/20-hall.jpg'
+  './imgsci/01-reflection.png',
+  './imgsci/02-viewtwist.png',
+  './imgsci/04-giantchair.png',
+  './imgsci/05-bodyblueprint.png',
+  './imgsci/06-sundial.png',
+  './imgsci/07-metalblock.png',
+  './imgsci/08-oscillatingbeam.png',
+  './imgsci/09-echotube.png'
   // ------------------------------------
 ];
 
